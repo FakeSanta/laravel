2c076d9ee07edf4501ad9661130659345ca24003
FakeSanta <aurelienfevrier08@gmail.com>
2023-11-13 15:25:00 +0100

    changes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    $users = User::where("name","Aurelien")->update(['email'=>'aurelienfevrier08@gmail.com']);
+    //$users = User::get();
+    dd($users);
 });
 
 Route::get('/dashboard', function () {
